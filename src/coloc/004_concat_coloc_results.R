0b3e07edce11a363256111339b388af033a950f5
piganoemi <nnp5@leicester.ac.uk>
2023-11-03 15:02:01 +0000

    added commands for genes for eqtlgen coloc

--- src/coloc/004_concat_coloc_results.R
+++ src/coloc/004_concat_coloc_results.R
@@ -5,7 +5,7 @@
 library(plyr)
 library(tidyverse)
 library(purrr)
-suppressMessages(library(xlsx))
+library(xlsx)
 
 ###############
 ### GTEx v8 ###
@@ -106,7 +106,7 @@ summary_fct = function(df) {
 tmp_1 <- lapply(data_list, summary_fct)
 nonnull <- sapply(tmp_1, typeof)!="NULL"  # find all NULLs to omit
 ID <- file_split$snp
-index_row <- 1:937
+index_row <- 1:937 #the length of tmp_1 which is the total nuber of all_susie.rds files
 tmp_2 <- map2(tmp_1, ID, ~cbind(.x, snp = .y))
 tmp_3 <- map2(tmp_2, index_row, ~cbind(.x, n_index = .y))
 tmp_4 <- tmp_3[nonnull]
@@ -137,7 +137,7 @@ write.xlsx(gene_gtex,"/alice-home/3/n/nnp5/PhD/PhD_project/Var_to_Gene/input/var
 ### eqtlGen ###
 ###############
 print("eqtlGen eQTL coloc results")
- .rds files containing standard coloc results with eQTLs
+# .rds files containing standard coloc results with eQTLs
 setwd("/scratch/gen1/nnp5/Var_to_Gen_tmp/results/eqtlgen/")
 file_paths_eqtlgen = list.files(pattern="_all_coloc.rds")
 
@@ -186,3 +186,69 @@ data_summary %>% filter(coloc) %>% select(snp, pheno, gene)
 
 # Save all results to file
 write_tsv(x=data_summary, file="/scratch/gen1/nnp5/Var_to_Gen_tmp/results/coloc_asthma_eqtlgen.tsv")
+
+# .rds files containing coloc.susie results with eQTLs
+file_paths_gtex = list.files(pattern="_all_susie.rds")
+
+# Manipulate file names to get various bits of info
+file_paths_gtex %>%
+  as_tibble %>%
+  mutate(file = gsub(x=value, pattern="_all_susie.rds", replacement="")) %>%
+  separate(col=file,
+           into=c("pheno", "chr", "pos", "a1", "a2", "t1"),
+           sep="_",
+           remove=FALSE) -> file_split
+
+# Clunky way to get out gene and tissue names from file names
+tmp = strsplit(file_split$file, "_")
+gene = sapply(tmp, tail, 1)
+tissue_fct = function(df) {
+  x = paste0(df[6])
+}
+tissue = sapply(tmp, tissue_fct)
+
+# Add gene and tissue to above dataframe
+file_split %>%
+  mutate(gene   = gene,
+         tissue = tissue) %>%
+  unite("snp", chr:a2) %>%
+  select(file=value, pheno, snp, gene, tissue) -> file_split
+
+# Read all .rds files
+data_list = lapply(file_paths_gtex, readRDS)
+
+# Function to extract coloc.susie information from results
+summary_fct = function(df) {
+  return(df$summary)
+}
+
+# Collapse list of coloc results into single dataframe and add a few new columns
+##Additional commands to work around summary with NULL data:
+tmp_1 <- lapply(data_list, summary_fct)
+nonnull <- sapply(tmp_1, typeof)!="NULL"  # find all NULLs to omit
+ID <- file_split$snp
+index_row <- 1:169 #the length of tmp_1 which is the total nuber of all_susie.rds files
+tmp_2 <- map2(tmp_1, ID, ~cbind(.x, snp = .y))
+tmp_3 <- map2(tmp_2, index_row, ~cbind(.x, n_index = .y))
+tmp_4 <- tmp_3[nonnull]
+
+file_split$n_index <- index_row
+
+data_summary_colocsusie = ldply(tmp_4) %>%
+  as_tibble %>%
+  left_join(file_split,by=c("snp","n_index")) %>%
+  select(-file) %>%
+  mutate(coloc_susie = if_else(PP.H4.abf >= 0.9, TRUE, FALSE)) %>%
+  arrange(desc(PP.H4.abf))
+
+# Check if there are any colocalisations
+data_summary_colocsusie %>% filter(coloc_susie) %>% select(snp, pheno, gene)
+
+
+# Save all results to file
+write_tsv(x=data_summary_colocsusie, file="/scratch/gen1/nnp5/Var_to_Gen_tmp/results/colocsusie_asthma_eqtlgen.tsv")
+
+#SAVE COLOC AND COLOC.SUSIE GENES INTO XLSX FILE:
+#coloc.susie does not have any significant colocalisation:
+gene_coloc <- as.data.frame(data_summary %>% filter(coloc) %>% select(gene))
+write.xlsx(gene_coloc,"/home/n/nnp5/PhD/PhD_project/Var_to_Gene/input/var2genes_raw.xlsx", sheetName = "eqtlGen_eQTL_genes", row.names=FALSE, col.names=FALSE, append=TRUE)
